795b6a74ba6ccad1ef38437a140c3d57d75619bf
Amir Ali <amir.saeed.ali92@gmail.com>
2020-10-08 16:49:48 +0500

    ts and tailwind setup

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from "next/app";
+import '../styles/tailwind.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className='md:flex bg-white rounded-lg p-24 justify-center'>
+      <div className="bg-indigo-700 h-56 w-20"></div>
+    </div>
+  );
+}
+
+export default Home;
