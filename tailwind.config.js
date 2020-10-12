47f74b91327bd44249310a73e77974ab4ed82dde
Amir Ali <amir.saeed.ali92@gmail.com>
2020-10-12 16:51:50 +0500

    sequential update of form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	purge: [
+		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [
+		require("@tailwindcss/ui")({
+			layout: "sidebar",
+		}),
+	],
+};
